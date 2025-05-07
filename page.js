bff0f5e1498d95820c7e5d68905c086d9df1ef62
张金晶 <2815757382@qq.com>
2025-05-07 12:17:57 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar"
+export default function Hello() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-indigo-300 bg-sky-50 text-7xl italic text-center font-stretch-ultra-condensed font-serif">Hello</h1>
+      <p className="text-indigo-500 bg-sky-50 font-serif">
+        Hello World
+      </p>
+      <p className="text-indigo-500 bg-sky-50 font-serif">
+        Hello World
+      </p>
+    </div>
+  )
+}
+
